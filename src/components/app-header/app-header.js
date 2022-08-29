4f8f122553aba090fa7632d6682d0c52c1f49002
Rostyslav Mysiuha <rossmysiuga@gmail.com>
2022-08-29 11:26:28 -0400

    Project structure, styles

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1>Rostyslav Mysiuha</h1>
+      <h2>5 records, liked 0 of them</h2>
+    </div>
+  )
+}
+
+export default AppHeader;
