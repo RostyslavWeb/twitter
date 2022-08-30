9fb758429871ff0d7a0919f8082bc0a4061b92c1
Rostyslav Mysiuha <rossmysiuga@gmail.com>
2022-08-30 11:56:35 -0400

    Class PostAddForm

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './app-header.css';
 
-const AppHeader = () => {
+const AppHeader = ({ liked, allPosts }) => {
   return (
     <div className="app-header d-flex">
       <h1>Rostyslav Mysiuha</h1>
-      <h2>5 records, liked 0 of them</h2>
+      <h2>{allPosts} records, liked {liked} of them</h2>
     </div>
   )
 }
